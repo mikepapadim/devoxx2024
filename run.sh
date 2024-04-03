6a50baedbfab1207aa30f7c3f32f5dec4deab144
mikepapadim <mikepapadim@hotmail.com>
2024-04-03 15:59:59 +0100

    Extend run.sh script to include examples using TensorAPI.

--- run.sh
+++ run.sh
@@ -1,2 +1,11 @@
-# Example 1
+# Example Tensor API
 tornado  -cp target/devoxx2024-1.0-SNAPSHOT.jar com.mikepapadim.devoxx.DemoTensorAPI
+
+# Example Tensor API - TornadoVM
+tornado --threadInfo -cp target/devoxx2024-1.0-SNAPSHOT.jar com.mikepapadim.devoxx.DemoTensorTornado
+
+# Example Tensor API - Vector API
+tornado  -cp target/devoxx2024-1.0-SNAPSHOT.jar com.mikepapadim.devoxx.DemoTensorVectorAPI
+
+# Example Tensor API - Onnx RT
+tornado  -cp target/devoxx2024-1.0-SNAPSHOT.jar com.mikepapadim.devoxx.DemoTensorAPIOnnxRT
