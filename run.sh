c7c49f4890dd181aa1413cf99c065809f0b70163
mikepapadim <mikepapadim@hotmail.com>
2024-04-03 15:25:13 +0100

    Add pom.xml, run.sh and set_paths.sh

---
+++ run.sh
@@ -0,0 +1,2 @@
+# Example 1
+tornado  -cp target/devoxx2024-1.0-SNAPSHOT.jar com.mikepapadim.devoxx.DemoTensorAPI
---
+++ set_paths.sh
@@ -0,0 +1,5 @@
+export TORNADO_ROOT=/home/mikepapadim/manchester/TornadoVM
+export TORNADO_SDK=${TORNADO_ROOT}/bin/sdk
+export DEVOXX_ROOT=${PWD}
+export PATH="${PATH}:${DEVOXX_ROOT}/bin"
+
